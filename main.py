36b6aeeddb023aa7ce525593d1dd11a32cda6bd7
Madeline Sharpe <50880124+Sharpaii@users.noreply.github.com>
2023-10-01 03:07:21 -0700

    Update main.py
    Finished code

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-person1_busy_schedule = [['12:00', '13:00'], ['16:00', '18:00']]
-person1_work_hours = ['9:00', '19:00']
-person2_busy_schedule = [['9:00','10:30'], ['12:20, 14:30'], ['14:30, 15:00'], ['16:00', '17:00']]
-duration_of_meeting = 30
-
+from datetime import datetime
 def combineSchedules(*inputs):
     output = []
     for schedule in inputs:
@@ -13,41 +9,137 @@ def combineSchedules(*inputs):
             output.append(schedule)
     return output
 
-def convertSchedules(input):
-    for item in input:
-        temp = []
-        for time in item:
-            if time[1] == ":":
-                temp.append(int(time[0]))
-                temp.append(int(time[2:4]))
-            else:
-                temp.append(int(time[0:2]))
-                temp.append(int(time[3:5]))
-        input[input.index(item)] = temp
+def bubbleSort(input):
+    for i in range(len(input)):
+        swap = False
+        for j in range(0, len(input) - i - 1):
+            time1 = [datetime.strptime(input[j][0], '%H:%M').time(), datetime.strptime(input[j][1], '%H:%M').time()]
+            time2 = [datetime.strptime(input[j + 1][0], '%H:%M').time(), datetime.strptime(input[j + 1][1], '%H:%M').time()]
+            if time1 > time2:
+                input[j], input[j + 1] = input[j + 1], input[j]
+                swap = True
+        if (swap == False):
+            break
+    return input
 
-def insertSort(input):
-    pass
+def mergeSchedules(input, type):
+    updatedSchedule = [input[0]]
+    index = 0
+    for i in range(len(input)):
+        startTime = datetime.strptime(input[i][0], '%H:%M').time()
+        endTime = datetime.strptime(input[i][1], '%H:%M').time()
+        maxTime = datetime.strptime(updatedSchedule[index][1], '%H:%M').time()
 
-def sortSchedules(input):
-    input.sort() # Change to insert sort
-    temp = []
-    for item in reversed(input):
-        if item[0][1] == ":":
-            temp.insert(0, input.pop(input.index(item)))
-    for item in reversed(temp):
-        input.insert(0, item)
+        if (type == "busy"):
+            if (startTime <= maxTime) and (endTime > maxTime):
+                updatedSchedule[index][1] = input[i][1]
 
-def updateSchedules(input):
-    pass
+            elif (startTime > maxTime):
+                index += 1
+                updatedSchedule.append(input[i])
+
+        elif(type == "work"):
+            if (startTime <= maxTime) and (endTime < maxTime):
+                updatedSchedule[index][1] = input[i][1]
+
+            elif (startTime > maxTime):
+                index += 1
+                updatedSchedule.append(input[i])
+
+    return(updatedSchedule)
+
+
+def getFreeSchedules(free, busy):
+    output = free
+    index = 0
+    for i in range(len(free)):
+        for j in range(len(busy)):
+            minTime = datetime.strptime(output[index][0], '%H:%M')
+            maxTime = datetime.strptime(output[index][1], '%H:%M')
+            startBlock = datetime.strptime(busy[j][0], '%H:%M')
+            endBlock = datetime.strptime(busy[j][1], "%H:%M")
+            if (startBlock == minTime):
+                output[index] = [busy[j][1], free[i][1]]
+
+            elif (startBlock <= maxTime):
+                output.append([busy[j][1], output[index][1]])
+                output[index][1] = busy[j][0]
+                index += 1
+    return(output)
+
+def verifyDuration(input, duration):
+    output = []
+    for i in range(len(input)):
+        timeA = datetime.strptime(input[i][0], '%H:%M')
+        timeB = datetime.strptime(input[i][1], '%H:%M')
+        if(int((timeB - timeA).total_seconds() // 60) >= duration):
+            output.append(input[i])
+    return output
 
 def debugPrint(input):
     for item in input:
         print(item)
     print("\n");
 
-busy = combineSchedules(person1_busy_schedule, person1_work_hours, person2_busy_schedule)
-sortSchedules(busy)
-debugPrint(busy)
-convertSchedules(busy)
-updateSchedules(busy)
-debugPrint(busy)
+def main():
+    busy = combineSchedules(person1_busy_Schedule, person2_busy_Schedule)
+    work = combineSchedules(person1_work_hours, person2_work_hours)
+
+    busy = bubbleSort(busy)
+    work = bubbleSort(work)
+
+    # print("All Busy Times")
+    # debugPrint(busy)
+    # print("All Work Times")
+    # debugPrint(work)
+
+    busy = mergeSchedules(busy, "busy")
+    work = mergeSchedules(work, "work")
+
+    # print("Condensed Busy Times")
+    # debugPrint(busy)
+    # print("Condensed Work Times")
+    # debugPrint(work)
+
+    work = getFreeSchedules(work, busy)
+    work = verifyDuration(work, duration_of_meeting)
+    print("Get Free Work Times")
+    debugPrint(work)
+
+# TEST CASE 1
+person1_busy_Schedule = [['12:00', '13:00'], ['16:00', '18:00']]
+person1_work_hours = ['9:00', '19:00']
+person2_busy_Schedule = [['9:00','10:30'], ['12:20', '14:30'], ['14:30', '15:00'], ['16:00', '17:00']]
+person2_work_hours = ['9:00', '18:30']
+duration_of_meeting = 30
+
+main()
+
+# TEST CASE 2
+person1_busy_Schedule = [['12:00','13:00'],['14:00','15:00']]
+person1_work_hours = ['9:00','19:00']
+person2_busy_Schedule = [['10:15','11:00'],['11:30','12:45'],['14:00','16:00']]
+person2_work_hours = ['9:00','18:30']
+duration_of_meeting = 30
+
+main()
+
+
+# TEST CASE 3
+person1_busy_Schedule = [['12:00','13:00'],['14:00','15:00']]
+person1_work_hours = ['9:00','19:00']
+person2_busy_Schedule = [['10:15','11:00'],['11:30','12:45'],['14:00','16:00']]
+person2_work_hours = ['9:00','18:30']
+duration_of_meeting = 120
+
+main()
+
+
+# TEST CASE 4
+person1_busy_Schedule = [['10:00','13:00'],['14:00','15:00']]
+person1_work_hours = ['9:00','19:00']
+person2_busy_Schedule = [['10:15','11:00'],['11:30','12:45'],['14:00','16:00']]
+person2_work_hours = ['9:00','18:30']
+duration_of_meeting = 60
+
+main()
